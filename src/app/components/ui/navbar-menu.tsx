a2373677619054155bb79223ef4010950c1b6580
Ulvi Nasibli <ulvi.nasibli@ulvi.nasibli-Mac>
2024-11-22 21:36:51 +0200

    change

--- src/app/components/ui/navbar-menu.tsx
+++ src/app/components/ui/navbar-menu.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import Image from "next/image";
+import { LinkProps } from 'next/link';
+
 
 const transition = {
   type: "spring",
@@ -109,11 +111,11 @@ export const ProductItem = ({
   );
 };
 
-export const HoveredLink = ({ children, ...rest }: any) => {
+export const HoveredLink = ({ children, ...rest }: React.PropsWithChildren<LinkProps>) => {
   return (
     <Link
       {...rest}
-      className="text-neutral-700 dark:text-neutral-200 hover:text-black "
+      className="text-neutral-700 dark:text-neutral-200 hover:text-black"
     >
       {children}
     </Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,11 @@
 
 import React from 'react'
 import "../globals.css";
-import { TextGenerateEffect } from './components/ui/text-generate-effect';
-import { ImagesSliderDemo } from './components/demo/sliderdemo';
-import { NavbarDemo } from './components/demo/navbarDemo';
 import { SparklesPreview } from "./components/demo/sparkles"
 import { TypewriterEffectSmoothDemo } from './components/demo/TypewriterEffectSmoothDemo';
 const page = () => {
   return (
     <div>
-      {/* <NavbarDemo /> */}
       <SparklesPreview />
       <br></br>
       <br></br>
